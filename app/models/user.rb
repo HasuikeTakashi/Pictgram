cc3718eceba067b758434a398cdd6357ef264b77
hasuike <t-k-s0402@ezweb.ne.jp>
2019-12-17 06:51:29 +0000

    04課題

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: { with:/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :password, length: { minimum: 8, maximum: 32 }, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  
+  has_secure_password
 end
+
+
